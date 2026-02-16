a8ad064769068ddc78b252cfe1fa3d472e807d9c
Bugrump <edmcdoughty@gmail.com>
2026-02-16 14:39:53 -0700

    E: Write file function and test added.

---
+++ functions/write_file.py
@@ -0,0 +1,32 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    working_dir_abs = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, file_path))
+    
+    a = os.path.dirname(file_path)
+    b = os.path.normpath(os.path.join(working_dir_abs, a))
+    d = os.path.normpath(os.path.join(working_directory, file_path))
+
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+    if valid_target_dir == False:
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    
+    directory_valid = os.path.isdir(os.path.join(working_dir_abs, file_path))
+    if directory_valid:
+        return f'Error: Cannot write to "{file_path}" as it is a directory'
+
+    try:
+        os.makedirs(b, exist_ok=True)
+    except Exception as e:
+        return f"Error: {e}"
+    
+    try:
+        with open(d, "w") as f:
+            f.write(content)
+    except Exception as g:
+        return f"Error: {g}"
+    
+    return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+
+    
---
+++ test_write_file.py
@@ -0,0 +1,8 @@
+from functions.write_file import write_file
+
+test1 = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+print(test1)
+test2 = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+print(test2)
+test3 = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+print(test3)
